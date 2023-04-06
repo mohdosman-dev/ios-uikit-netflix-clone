59fb7612136ba2232d40030347fd93be00c3c035
Mohammed Osman <mohammed.osman@vianeos.com>
2023-04-06 10:56:57 +0400

    Initial Commit

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Mohammed Osman on 06/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
